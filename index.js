e30f7ce913cc9d908cc6f54ab783908dc0dc9829
Bradley Flood <bradley.flood@foxsports.com.au>
2017-10-13 12:21:31 +1100

    logic 1.0.0

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+/**
+ * Calculate the superfactorial of a number
+ * @param {number} num to calculate
+ * @returns {number} calculated value
+ */
 const superfactorial = (num) => {
-	// Type check
-	// Range check
-	// Calculate
-	return num;
-};
+	if (!Number.isInteger(num))
+		throw new TypeError('I need a number!');
 
-const factorial = (num) => {
-	// Calculate factorial
+	if (num < 0)
+		throw new RangeError('Factorials must be positive whole numbers');
+
+	if (num === 0)
+		return 1;
+
+	let superFact = 1;
+	for (let i = num; i > 1; i--) {
+		let fact = 1;
+		for (let j = i; j > 1; j--) {
+			fact *= j;
+		}
+		superFact *= fact;
+	}
+
+	return superFact;
 };
 
 module.exports = superfactorial;
